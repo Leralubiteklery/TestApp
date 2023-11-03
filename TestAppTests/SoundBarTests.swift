e94ca35f645aa321402d332a9b71ef7ded6d1cab
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-11-03 14:46:26 +0300

    Added unit tests for the setVolume method.

---
+++ TestAppTests/SoundBarTests.swift
@@ -0,0 +1,42 @@
+//
+//  TestAppTests.swift
+//  SoundBarTests
+//
+//  Created by Lera Savchenko on 3.11.23.
+//
+
+import XCTest
+@testable import TestApp
+
+final class SoundBarTests: XCTestCase {
+
+    var sut: SoundBar!
+    
+    override func setUp()  {
+        super.setUp()
+        sut = SoundBar()
+    }
+
+    override func tearDown()  {
+        sut = nil
+        super.tearDown()
+    }
+
+    func testSetVolumeShouldReturn0WhenLowestVolume()  {
+        sut.setVolume(to: -1)
+        XCTAssert(sut.volume == 0, "Lowest volume should be equal 0")
+    }
+    
+    func testSetVolumeShouldReturn100WhenHighestVolume() {
+        sut.setVolume(to: 100)
+        XCTAssert(sut.volume == 100, "Highest volume should be equal 100")
+    }
+
+    func testPerformanceExample()  {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
