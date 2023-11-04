549073c5a0ab6db124a94130624fc1278a4ec424
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-11-04 10:06:22 +0300

    Enabled test coverage display.

--- TestAppTests/SoundBarTests.swift
+++ TestAppTests/SoundBarTests.swift
@@ -32,11 +32,4 @@ final class SoundBarTests: XCTestCase {
         XCTAssert(sut.volume == 100, "Highest volume should be equal 100")
     }
 
-    func testPerformanceExample()  {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
